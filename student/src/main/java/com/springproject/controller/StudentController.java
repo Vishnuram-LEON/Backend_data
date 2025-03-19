ea402fd5eb0dd45a2ee1f00ac6ef60ddad2706ed
Kesavaraj6241 <kesavaraj6241@gmail.com>
2025-03-19 18:03:19 +0530

    student project

---
+++ student/src/main/java/com/springproject/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.springproject.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+System.out.println("STUDENT CONTROLLER");
+}
